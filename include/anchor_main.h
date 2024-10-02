f4b2ccebf3b04c31b4fb989299423ca5adb505cc
Zeki <nguyenhatrung411@gmail.com>
2024-10-02 16:30:56 +0900

    Update

---
+++ include/anchor_main.h
@@ -0,0 +1,7 @@
+#ifndef __ANCHOR_MAIN_H__
+#define __ANCHOR_MAIN_H__
+
+extern void anchor_main(void);
+extern void anchor_simple_rx_main(void);
+
+#endif // __ANCHOR_MAIN_H__
---
+++ include/tag_main.h
@@ -0,0 +1,8 @@
+#ifndef __TAG_MAIN_H__
+#define __TAG_MAIN_H__
+
+
+extern void tag_main(void);
+extern void tag_simple_tx_main(void);
+
+#endif // __TAG_MAIN_H__
